b3c570b941667dfe375c041eab6e9896c077f26e
karaAJC <KaraAJC@gmail.com>
2017-12-06 21:44:16 -0600

    updates to railsrc

--- rails.md
+++ rails.md
@@ -1,12 +1,10 @@
 # Rails setup
 ## Rails Specs
 - Verion: 5.0.2
-- Rails New Script: rails new _repo_ -d=postgresql -T 
+- Rails New Script: rails new _repo_ -d=postgresql -T
 ## Rails Gems I use
 - RSPEC
 - Shoulda Matchers
-- Omniauth
 - Devise
 - Materialize-rails
-- Cloudinary
 - cancancan
--- rails_template.rb
+++ rails_template.rb
@@ -1,33 +1,33 @@
 # ~/rails_template.rb
 
 gem_group :development, :test do
-  gem "capybara"
-  gem "factory_girl_rails"
-  gem "rspec-rails
-  gem "shoulda-matchers"
-  gem "faker"
+  gem 'capybara'
+  gem 'factory_bot_rails'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+  gem 'faker'
 end
 
 gem_group :production, :staging do
-  gem "rails_12factor"
+  gem 'rails_12factor'
 end
 
 scss = <<-SCSS
-@import "*";
+  @import '*';
 SCSS
 
 run "echo '#{scss}' >> app/assets/stylesheets/application.scss"
-run "rm app/assets/stylesheets/application.css"
-run "rm README.rdoc"
+run 'rm app/assets/stylesheets/application.css'
+run 'rm README.rdoc'
 run "echo '# #{@app_name.titleize}' >> README.md"
 
-run("bundle install")
+run('bundle install')
 
-generate("rspec:install")
-rake("db:create")
+generate('rspec:install')
+rake('db:create')
 
 run "echo 'require \"capybara/rails\"' >> spec/rails_helper.rb"
 
 git :init
-git add: "."
-git commit: "-a -m initial setup"
+git add: '.'
+git commit: '-a -m initial setup'
